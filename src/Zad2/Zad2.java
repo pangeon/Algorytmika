8c344637683b71b6b8f4acb9b3fbc8605ca534a9
Kamil <Kamil@DESKTOP-8ISSH51>
2017-03-18 17:30:33 +0200

    18.03.2017 | 18:30 - Zad.2 (zakończono)

--- src/Zad2/Zad2.java
+++ src/Zad2/Zad2.java
@@ -10,25 +10,61 @@ public class Zad2 {
         do momentu gdy ich suma nie przekroczy zadanej liczby.
         dla podanych niżej wartości:
         */
-        int ilw = 500; 
-        int max= 100;
+        int czy_parzysta = 2; //jeśli 1 - nieparzysta
+        int ilw = 500;
+        int ilw_2 = 20;
+        int max = 100/czy_parzysta;
         int[] rob = new int[ilw];
+        int[] rob_2 = new int[ilw_2];
         
         /*
         jeżeli zmniejszymy długość wektora z 500 do 20 
         program nie działa poprawnie. Dlaczego tak jest 
         i jak to poprawić?
         */
-        
-        for (int j = 0; j < ilw; j++) {   
-            rob[j] = (int)(max*Math.random());
-            if(rob[j] % 2 == 0) {
-                System.out.println("ilw = " + ilw + " | max = " + max + " | rob = " + rob[j]);
-            }
+        System.out.println("Losowe liczby ilość: " + ilw_2 + " maks. wart: " + max*czy_parzysta);
+        System.out.println("Wektor 1 - losowe liczby parzyste (1-500):");
+        for (int j = 0; j < ilw; j++) {
+            //chcąc uzyskać liczby parzyste mnoże wynik przez 2
+            //suma liczb nieparzystych daje liczbe parzystą
+            //suma liczb parzystych daje liczbę parzystą
+            rob[j] = ((int)(max*Math.random()))*czy_parzysta;     
+            System.out.print(" [" + j + "]=" + rob[j]);
         }
-        // nie rozumiem gdzie szukać problemu.
-        
-        
-    }
-    
-}
+        // generuje losowo liczby parzyste
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Losowe liczby ilość: " + ilw_2 + " maks. wart: " + max*czy_parzysta);
+        System.out.println("Wektor 2 - losowe liczby parzyste (1-20):");
+        for (int j = 0; j < ilw_2; j++) {
+            rob[j] = ((int)(max*Math.random()))*czy_parzysta; //tworzy wektor
+            System.out.print(" [" + j + "]=" + rob[j]); //wyświetla jego część
+        }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Losowe liczby ilość: " + ilw_2 + " maks. wart: " + max*czy_parzysta);
+        System.out.println("Wektor 3 - losowe liczby nieparzyste i parzyste (1-20):");
+        for (int j = 0; j < ilw_2; j++) {
+            czy_parzysta = 1;
+            rob[j] = ((int)(max*Math.random()))*czy_parzysta; //tworzy wektor
+            System.out.print(" [" + j + "]=" + rob[j]); //wyświetla jego część
+        }
+        //do wektora 4 przypisujemy liczby parzyste wektora 3
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Wektor 4 - liczby parzyste i zera || wektor na bazie wektora 3:");
+        System.out.println("Liczby nieparzyste przyjmują wart 0");
+        System.out.println("Losowe liczby ilość: " + ilw_2 + " maks. wart: " + max*czy_parzysta);
+        for (int i = 0; i < ilw_2; i++) {
+            if (rob[i] % 2 == 0) {
+                    rob_2[i] = rob[i];
+                }
+                System.out.print(" [" + i + "]=" + rob_2[i]);
+                int count = rob_2.length;
+            if (i == 19) {
+                    System.out.println("");
+                    System.out.println("dłogość wektora: " + count);
+                }
+            }
+        } //rozpisanie zajęło trochę czasu ale nie znazłem problemu !
+    }        
