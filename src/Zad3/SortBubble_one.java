4c8478b3b7e1282ff5548b798a811a5d96beb70f
Kamil <Kamil@DESKTOP-8ISSH51>
2017-03-18 20:08:06 +0200

    18.03.2017 | 21:06 - Realizacja Zad3, problem z SortBubble_two

--- src/Zad3/SortBubble_one.java
+++ src/Zad3/SortBubble_one.java
@@ -13,39 +13,57 @@ package Zad3;
 class SortBubble_one{
 
     public static void main (String Args[]) {
-        int ilw = 15;
-        int[] wek = new int[ilw];
-        int i,j;
-        int pom;
-        boolean zamieniono;
+        int dl_vector = 15;
+        int[] wek = new int[dl_vector];
+        int i = 0;
+        int j = 0; //określa sumę przesortowań
+        int pom = 0;
+        boolean flaga = true;
+        int test = 0;
 
         //generuje wektor
-        for (i=0;i<ilw;i++)
-                wek[i]=(int)(100*Math.random());	
-        //wypisuje wygenerowany wektor
-        System.out.println("Wektor danych: ");
-        for (i=0;i<ilw;i++)
-            System.out.print(wek[i]+ "  ");
+        for (i=0;i<dl_vector;i++) {
+            wek[i]=(int)(100*Math.random());
+        }
+        //wypisuje wygenerowany wektor (wart. 0-100)
+        System.out.println("Wektor bazowy: ");
+        for (i=0;i<dl_vector;i++) {
+            System.out.print(" [" + i + "] = " + wek[i]+ " | ");
+        }
         System.out.println();
 
         //sortowanie
+        System.out.println("Początek sortowania...");
+        double a = System.currentTimeMillis();
+        
         do {
-            zamieniono=false;
-            for (i=1;i<ilw;i++){
+            flaga=false;
+            for (i=1;i<dl_vector;i++){
+                System.out.print(" [" + i + "] = " + wek[i]+ " |"); //wyniki kolejnych przesortowań
+                
+                if (i == dl_vector-1) {
+                    System.out.println(""); //niezbędne do łamania linii
+                }
                 if (wek[i]<wek[i-1]){
                     pom = wek[i];
                     wek[i] = wek[i-1];
                     wek[i-1] = pom;
-                    zamieniono=true;
+                    flaga=true;
                 }
             }
+            j++; //suma przeortowań - inkrementacja przy każdym obrocie pętli
         }
-        while (zamieniono);
+        while (flaga);
+        System.out.println("ilość przesortowań: " + j);
+        double b = System.currentTimeMillis();
+        System.out.println("Czas sortowania: " + (b - a));
+        System.out.println("Koniec sortowania...");
 
         //wypisuje posortowany wektor
+        //czyli podstawia pod indexy rosnące wartości wektora bazowego
         System.out.println("Wektor posortowany: ");
-        for (i=0;i<ilw;i++)
-            System.out.print(wek[i]+ "  ");
-
-    }	
+        for (i=0;i<dl_vector;i++) {
+            System.out.print(" [" + i + "] = " + wek[i]+ " | ");
+        }
+    } // sortowanie mało efektywne
 }
--- src/Zad3/SortBubble_two.java
+++ src/Zad3/SortBubble_two.java
@@ -13,34 +13,52 @@ package Zad3;
 class SortBubble_two{
 
     public static void main (String Args[]) {
-    int ilw = 15;
-    int[] wek = new int[ilw];
-    int i,j;
-    int pom;
+        int ilw = 15;
+        int[] wek = new int[ilw];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int pom = 0;
 
-    //generuje wektor
-    for (i=0;i<ilw;i++)
-            wek[i]=(int)(100*Math.random());	
-    //wypisuje wygenerowany wektor
-    System.out.println("Wektor danych: ");
-    for (i=0;i<ilw;i++)
-        System.out.print(wek[i]+ "  ");
-    System.out.println();
-
-    //sortowanie
-    for (i=1;i<ilw;i++){
-        for (j=ilw-1;j>=i;j--){
-            System.out.print(j+" ");
-            if (wek[j-1]>wek[j]){
-                pom = wek[j-1];
-                wek[j-1] = wek[j];
-                wek[j] = pom;
-            }
+        //generuje wektor
+        for (i=0;i<ilw;i++) {
+            wek[i]=(int)(100*Math.random());
         }
-    }
-    //wypisuje posortowany wektor
-    System.out.println("Wektor posortowany: ");
-    for (i=0;i<ilw;i++)
-        System.out.print(wek[i]+ "  ");
+        //wypisuje wygenerowany wektor
+        System.out.println("Wektor bazowy: ");
+        for (i=0;i<ilw;i++) {
+            System.out.print(" [" + i + "] = " + wek[i]+ " | ");
+        }
+        System.out.println();
+
+        //sortowanie
+        System.out.println("Początek sortowania...");
+        double a = System.currentTimeMillis();
+        for (i=1;i<ilw;i++){ 
+            System.out.print(wek[i]+" ");
+            for (j=ilw-1;j>=i;j--) {
+                System.out.print(j+" ");
+                if (j == ilw-1) {
+                    System.out.println("");
+                }
+                if (wek[j-1]>wek[j]){
+                    pom = wek[j-1];
+                    wek[j-1] = wek[j];
+                    wek[j] = pom;
+                    k++;
+                }
+            } 
+        }
+        System.out.println("");
+        System.out.println("ilość przesortowań: " + k);
+        double b = System.currentTimeMillis();
+        System.out.println("Czas sortowania: " + (b - a));
+        System.out.println("Koniec sortowania...");
+        
+        //wypisuje posortowany wektor
+        System.out.println("Wektor posortowany: ");
+        for (i=0;i<ilw;i++) {
+            System.out.print(" [" + i + "] = " + wek[i]+ " | ");
+        }
     }	
 }
