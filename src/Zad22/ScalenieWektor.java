4d2128efac618f543bed1fcdbf5eb6af532cebb5
Kamil <Kamil@Cecherz>
2017-05-15 22:16:18 +0200

    15-05-2017 | Zad 22 + metoda łącząca wektory, - metoda sortująca

--- src/Zad22/ScalenieWektor.java
+++ src/Zad22/ScalenieWektor.java
@@ -19,34 +19,53 @@ package Zad22;
 public class ScalenieWektor {
 
     public static void main(String[] args) {
+        // identyczność zadeklarowanych nazw jak parametrów metody 
+        // sort_array jest przypadkowa
         int[] wek = {13,57,89,95};
         int[] wek1 = {25,56,58,67,69,70,76};
         
-        int dl_wek = wek.length; // ilość elem. wek
-        int dl_wek1 = wek1.length; // ilość elem. wek1
-        int sum_dl_wek = dl_wek + dl_wek1; // suma elem. obu wek.
-        System.out.println("dl_wek: " + dl_wek);
-        System.out.println("dl_wek1: " + dl_wek1);
-        System.out.println("sum_dl_wek: " + sum_dl_wek);
-                
-        int[] wek2 = new int[sum_dl_wek];
-        
+        System.out.println("Metoda sum_array(): ");
+        sum_array(wek, wek1, true); // suma dwóch zadeklarowych wcześniej tablic
+        System.out.println("Metoda sort_array(): ");
+        sort_array(sum_array(wek, wek1, false)); // sortowanie sumy wektorów
+    }
+    /* metoda sumująca wektory */
+    static int[] sum_array(int[] wek, int[] wek1, boolean flag) {
+        // dadaje dł. wektorów by otrzymać rozmiar nowej tablicy
+        int dl_wek = wek.length + wek1.length;
+        // inicjuje nową tablice o zdefiniowanym rozmiarze
+        int[] wek2 = new int[dl_wek];
         for (int i = 0; i < dl_wek; i++) {
-            wek2[i] = wek[i];
-            if (i==0) {
-                System.out.println("wek2: ");
+            // przypisuje elementy z tablicy pierwszej do trzeciej
+            // numer ostatniego indexu - 1 to jej rozmiar
+            if (i < wek.length) {  
+                wek2[i] = wek[i];
+            // przypisuje elementy z tablicy drugiej do trzeciej
+            // w przypadku numeru indexu większego lub równego
+            // rozmiarowi pierwszej tablicy
+            } else {
+                wek2[i] = wek1[i-wek.length];
             }
-            System.out.print(" [ " + i + " ] " + wek2[i]);
+            // w celu uniknięcia podwójnego drukowania elementów tablicy 
+            // podczas testowania
+            if(flag == true) { 
+                System.out.println(" [ " + i + " ] " + wek2[i]);
+            }
         }
-//        int j = 0;
-//        for (int i = 3; i < dl_wek1; i++) {           
-//            
-//            wek2[i] = wek1[j];
-//            if (i==0) {
-//                System.out.println("wek2: ");
-//            }
-//            j += i;
-//            System.out.print(" [ " + i + " ] " + wek2[i]);
-//        }
-    }        
+        return wek2;      
+    }
+    /* metoda sortująca wektor */
+    static void sort_array(int[] tab) {
+        for (int i = 0; i < tab.length; i++) {        
+            if (tab[i] > tab[i+1]) {     
+                // tab[1] - 56 > tab[2] - 23
+                // tab[2-1] | tab[1+1]
+                int temp; // ten większy
+                temp = tab[i]; // zapisuje go
+                tab[i+1] = temp; // przenoszę do kolejnego indexu
+                tab[i] = tab[i+1]; // ten mniejszy przenoszę na dół         
+            }
+            System.out.println(" [ " + i + " ] " + tab[i]);
+        }
+    }
 }
